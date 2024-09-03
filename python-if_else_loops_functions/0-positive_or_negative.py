c955023944e3535b1244636a25cb3b33bd323cab
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-03 15:10:08 +0200

    removing elif and else

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,8 +5,8 @@ number = random.randint(-10, 10)
 if number > 0:
     print(f"{number} is positive ")
 
-elif number == 0:
+if number == 0:
     print(f"{number} is zero")
 
-else:
+if number < 0:
     print(f"{number} is negative")
