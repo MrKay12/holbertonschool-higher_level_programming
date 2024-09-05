da2e4a7f227382aa3df62adfb61ca4fbc65662a3
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-05 16:11:33 +0200

    prints the result of the addition of all arguments

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+
+i = 1
+sum = 0
+argc = len(argv)
+
+if __name__ == "__main__":
+    while i < argc:
+        sum += int(argv[i])
+        i += 1
+    print("{}".format(sum))
