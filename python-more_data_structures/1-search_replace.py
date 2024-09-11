f8d5e8b40132a7a31fe428458229eebab4104581
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-11 10:22:03 +0200

    change little thing for pep8

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
 
-    new_list = [replace if element == search else element for element in my_list]
+    list2 = [replace if element == search else element for element in my_list]
+    new_list = list2
     return new_list
 
-search_replace = __import__('1-search_replace').search_replace
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
