3d3a2a9c707b3a7f31498892100c58a9d182b6e6
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-11 10:12:21 +0200

    changing name of variable

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
 
-    list2 = [replace if element == search else element for element in list2]
-    return list2
+    new_list = [replace if element == search else element for element in my_list]
+    return new_list
+
+search_replace = __import__('1-search_replace').search_replace
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
