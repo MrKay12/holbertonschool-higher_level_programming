7d2774839327fc7278bf3e4e86fc6fa129148e8b
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-11 09:42:31 +0200

    compression of code to see if it's better

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-
-    matrix2 = []
-
-    for row in matrix:
-        row2 = [element ** 2 for element in row]
-        matrix2.append(row2)
-
-        return matrix2
+    
+    return [[element ** 2 for element in row] for row in  matrix]
