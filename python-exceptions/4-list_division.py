f14663899bfee73a44045c39b9a43d40b57ab4c8
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-12 17:18:09 +0200

    pep8

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    
+
     for i in range(list_length):
         try:
             num1 = my_list_1[i]
@@ -18,5 +18,5 @@ def list_division(my_list_1, my_list_2, list_length):
             div = 0
         finally:
             result.append(div)
-    
+
     return result
