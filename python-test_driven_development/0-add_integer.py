af94c242bfd8c3234fb83dd84f227e636f07bc6d
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-13 13:13:45 +0200

    change some desciption of code + change test for code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
+""" 
+Define function for integer addition
+"""
+
 def add_integer(a, b=98):
-    """
-    Adds two integers.
-    Parameters:
-    a (int, float): The first number to add.
-    b (int, float, optional): The second number to add. Defaults to 98.
-    Raises:
-    TypeError: If a or b is not an integer or float.
-    Returns:
-    int: The sum of a and b, after casting floats to integers.
+    """Adds two integers.
+
+    Raises: TypeError: If a or b is not an integer or float.
+    Returns: int: The sum of a and b, after casting floats to integers.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
