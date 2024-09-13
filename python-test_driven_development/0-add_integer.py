ba9e027e7742bc5946779b07bd4c7ef8a64f7dd3
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-13 11:59:52 +0200

    pep8

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,14 +2,11 @@
 def add_integer(a, b=98):
     """
     Adds two integers.
-
     Parameters:
     a (int, float): The first number to add.
     b (int, float, optional): The second number to add. Defaults to 98.
-
     Raises:
     TypeError: If a or b is not an integer or float.
-
     Returns:
     int: The sum of a and b, after casting floats to integers.
     """
@@ -19,18 +16,3 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
-
-add_integer = __import__('0-add_integer').add_integer
-
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
