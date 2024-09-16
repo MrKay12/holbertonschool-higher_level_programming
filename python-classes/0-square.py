4b8fc57491d6299e9135330c5c9d25eb8d805aea
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-16 09:51:02 +0200

    empty class Square

---
+++ python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Class that represent a square.
+    
+    This class is empty cause no attribute or method, is define as a place holder for future fonctionality
+    """
+    pass
