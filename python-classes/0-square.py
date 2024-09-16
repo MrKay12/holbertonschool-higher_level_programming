a8da721d20d019d779704d47313cd6757d7a6654
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-16 10:02:10 +0200

    pep8

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,11 +2,11 @@
 """Define a class for Square"""
 
 
-
 class Square:
     """
     Class that represent a square.
-    
-    This class is empty cause no attribute or method, is define as a place holder for future fonctionality
+
+    This class is empty cause no attribute or method,
+    is define as a place holder for future fonctionality
     """
     pass
