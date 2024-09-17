5e7a99a39de94841214934df72af5e45b91ff868
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-17 13:44:46 +0200

    empty Rectangle class

---
+++ python-more_classes/0-rectangleUntitled-1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a class for Square"""
+
+
+class Rectangle:
+    """
+    Class that represent a square.
+
+    This class is empty cause no attribute or method,
+    is define as a place holder for future fonctionality
+    """
+    pass
