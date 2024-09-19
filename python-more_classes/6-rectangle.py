a974533be712cae1b28e83cdaa913480d62a7d71
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-19 13:22:42 +0200

    swap order in init

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -10,9 +10,9 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
