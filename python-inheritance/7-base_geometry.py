ccd345c77da9a7c842e079d9df42a0f48b0717d6
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-23 20:29:15 +0200

    changing doc to see if checker is happy

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -19,7 +19,6 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
-
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
