c4af983e3895002c1fa69d93b9aab69d09f6532f
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-23 11:22:14 +0200

    going 1 step back + adding more doc

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,5 +6,12 @@ from the specified class; otherwise False
 
 
 def inherits_from(obj, a_class):
-    """Check if obj is an instance of a class that inherited from a_class."""
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    """Check if obj is an instance of a class that inherited from a_class.
+     Args:
+        obj: The object to check.
+        a_class: The class to check against.
+
+    Returns:
+        bool: True if obj is an instance or inherited instance of a_class,
+                False otherwise."""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
