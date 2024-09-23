4d0a727be0d9d95fc426a859b6f7abfdba963efb
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-23 11:16:37 +0200

    swap sub by inst

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ from the specified class; otherwise False
 
 def inherits_from(obj, a_class):
     """Check if obj is an instance of a class that inherited from a_class."""
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
