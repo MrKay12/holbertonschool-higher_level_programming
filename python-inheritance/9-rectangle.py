cbf2927586977b0bdb6e3fa285ca53e10c0ece0e
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-24 10:48:18 +0200

    supp a white space

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -5,7 +5,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry."""
-
     def __init__(self, width, height):
         """Initialize with width and height after validation.
 
