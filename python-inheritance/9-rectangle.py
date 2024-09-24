f856da610268b36b91811c4c9e9b837cf56449d7
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-24 09:53:42 +0200

    get rid off BaseGeometry + importing it

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
-""" define a class BaseGeometry"""
-
-
-class BaseGeometry:
-    """Class with geometry utilities."""
-
-    def area(self):
-        """Raise an exception for unimplemented area method."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate if value is an integer greater than 0."""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry."""
