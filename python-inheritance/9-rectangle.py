182438b1e198cfeacf1ef89f710c4b44792ccec8
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-24 11:22:59 +0200

    changing return for better use by other class

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -23,4 +23,6 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Return the rectangle position"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
