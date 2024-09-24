8b2f3af471695d189fecb498f1b71f24fb9501dd
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-24 09:46:00 +0200

    adding def area/str to rectangle class in code of previous task

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class with geometry utilities."""
+
+    def area(self):
+        """Raise an exception for unimplemented area method."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate if value is an integer greater than 0."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize with width and height after validation."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """return the area of rectangle"""
+        return self.__width * self.__height
+    
+    def __str__(self):
+        """Return the rectangle position"""
+        return f"[rectangle] {self.__width}/{self.__height}"
