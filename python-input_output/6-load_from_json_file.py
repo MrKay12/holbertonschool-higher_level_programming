bbf3d5e6730a97dfd6a3e4d734774309fe90c2b0
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-30 12:39:51 +0200

    adding doc

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""Args:
+        filename (str): The name of the file to load the JSON data from
+
+    Returns:
+        object: The Python object created from the JSON data"""
 
 import json
 
+
 def load_from_json_file(filename):
+    """ Creates an object from a JSON file"""
     with open(filename, 'r') as f:
         return json.load(f)
