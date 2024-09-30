23a46a54fcf7e0904880084f38cf818a30b90448
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-30 12:30:25 +0200

    switch place between doc and import

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
-import json
-
 """Args:
         my_obj: The Python object to convert to a JSON string.
 
     Returns:
         str: The JSON representation of the object."""
 
+import json
+
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object (string).    """
+    """Returns the JSON representation of an object (string)."""
     return json.dumps(my_obj)
