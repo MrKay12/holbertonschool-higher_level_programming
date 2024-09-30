ec4d2efa4cd6c778394e89ecba30edb9dab93cca
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-30 12:16:49 +0200

    adding doc

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+
+    Returns:
+        int: The number of characters added to the file.
+    """
+
 
 def append_write(filename="", text=""):
     with open(filename, 'a', encoding='utf-8') as f:
