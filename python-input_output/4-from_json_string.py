64386c4ec5c3b9523bbfa455ae945461c8942bf3
MrKay12 <quentin.lepoutre12@gmail.com>
2024-09-30 12:37:16 +0200

    adding doc

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+""" Args:
+        my_str (str): The JSON string to convert into a Python object.
+
+    Returns:
+        object: The Python data structure represented by the JSON string."""
 
 import json
 
+
 def from_json_string(my_str):
+    """Returns an object represented by a JSON string"""
     return json.loads(my_str)
