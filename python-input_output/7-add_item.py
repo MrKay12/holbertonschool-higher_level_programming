7a18b3c94a78f22951a4640fe3c5cd3191a29ee0
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-01 09:47:46 +0200

    adding if __name__

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,15 +8,16 @@ If the file does not exist, it initializes an empty list.
 import sys
 import json
 
+if __name__ == "__name__":
+   
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    filename = "add_item.json"
+    try:
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
 
-filename = "add_item.json"
-try:
-    my_list = load_from_json_file(filename)
-except FileNotFoundError:
-    my_list = []
-
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, filename)
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, filename)
