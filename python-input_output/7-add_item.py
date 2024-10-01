1a042f505290150b2dbb5e9d77c83873a51e4f4d
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-01 10:31:49 +0200

    adding doc for see if checker are happy

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,6 +9,7 @@ import sys
 import json
 
 if __name__ == "__main__":
+    """insert some doc here"""
 
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
