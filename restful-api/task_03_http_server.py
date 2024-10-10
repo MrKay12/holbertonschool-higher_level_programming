b92cecef3f73b140b572096fd478a56f7141af13
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-10 11:57:27 +0200

    putting self. in every if/elif at the place ate the begin

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -7,21 +7,29 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class SimpleAPIHandler(BaseHTTPRequestHandler):
     """testing if we need documentation"""
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-
         if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
+            
         elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
             response = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(response).encode('utf-8'))
+            
         elif self.path == '/status':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
             self.wfile.write(b"OK")
+            
         else:
             self.send_response(404)
+            self.end_headers()
             self.wfile.write(b"Endpoint not found")
-
-        self.wfile.write(json.dumps(response).encode('utf-8'))
 
 
 def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
