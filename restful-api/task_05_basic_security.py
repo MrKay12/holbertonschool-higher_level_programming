26820ce910664d10765301d874f79e1509fd0301
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-17 10:07:41 +0200

    Update task_05_basic_security.py
    Changing error message and more

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -45,7 +45,7 @@ def login():
     if username in users and check_password_hash(users[username]['password'], password):
         access_token = create_access_token(identity={"username": username, "role": users[username]["role"]})
         return jsonify(access_token=access_token), 200
-    return "Invalid credentials", 401
+    return jsonify({"error": "Invalid credentials"}), 401
     
 @app.route('/jwt-protected', methods=['GET'])
 @jwt_required()
@@ -57,12 +57,12 @@ def jwt_protected():
 def admin_only():
     current_user = get_jwt_identity()
     if current_user['role'] != 'admin':
-        return "Admin access required", 403
+        return jsonify({"error": "Admin access required"}), 403
     return "Admin Access: Granted"
     
  @jwt.unauthorized_loader
   def handle_unauthorized_error(err):
-      return jsonify({"Missing or invalid token"}), 401
+      return jsonify({"error": "Missing or invalid token"}), 401
 
   @jwt.invalid_token_loader
   def handle_invalid_token_error(err):
