52b1d8d404a18ef7b4d264d32577b8d43edaedec
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-21 16:35:58 +0200

    **** it

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,12 @@
 -- create a table 
-create table if not exists second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
 -- Insert multiple row
-insert into second_table (id, name, score) values
+INSERT INTO second_table (id, name, score) VALUES
 (1, "Jhon", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
