c3358ea7a1e260b4783307827b0c8fdb44bc1600
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-21 16:25:29 +0200

    trying things

--- SQL_introduction/9-full_creation.sql.sql
+++ SQL_introduction/9-full_creation.sql.sql
@@ -1,9 +1,5 @@
 -- create a table 
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 -- Insert multiple row
 INSERT INTO second_table (id, name, score) VALUES
