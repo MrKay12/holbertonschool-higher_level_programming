64ca1bc868b71b469ca6d318436ff615bdfe2bb3
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-30 13:48:01 +0100

    Write a script that creates the MySQL server user user_0d_1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- script that creates the MySQL server user user_0d_1
+create DATABASE IF not exists hbtn_0c_0;
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT privileges on hbtn_0c_0 TO 'user_0d_1'@'localhost';
