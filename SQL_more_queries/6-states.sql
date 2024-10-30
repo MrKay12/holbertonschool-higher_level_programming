bb746f59fc08bec72b666c40e7c9b6b711060f75
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-30 14:58:48 +0100

    Upperscale

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 --creates the database hbtn_0d_usa and the table states
-create DATABASE IF not exists hbtn_0d_usa;
-create table if not exists hbtn_0d_usa.states (
+create DATABASE IF NOT EXISTS hbtn_0d_usa;
+create TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) not null
+    name VARCHAR(256) NOT NULL
 );
