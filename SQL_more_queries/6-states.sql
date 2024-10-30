7fa07dccf1a71ae0df3cfd05bd3c1fb4dcf32064
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-30 14:54:28 +0100

    changing table name

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 --creates the database hbtn_0d_usa and the table states
 create DATABASE IF not exists hbtn_0d_usa;
-create table if not exists states (
+create table if not exists hbtn_0d_usa.states (
     id INT IDENTITY(1,1) UNIQUE not null PRIMARY KEY,
     name VARCHAR not null(256)
 );
