4fbe4929ab2b3721712d38cef193604cb2031abb
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-30 15:00:49 +0100

    UPPER again

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 --creates the database hbtn_0d_usa and the table states
-create DATABASE IF NOT EXISTS hbtn_0d_usa;
-create TABLE IF NOT EXISTS hbtn_0d_usa.states (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL
 );
