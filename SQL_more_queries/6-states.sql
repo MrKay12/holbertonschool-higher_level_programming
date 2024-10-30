687dc3414f49eb463bf679f442cfe64b2fc7283a
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-30 14:57:00 +0100

    changing the int

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 --creates the database hbtn_0d_usa and the table states
 create DATABASE IF not exists hbtn_0d_usa;
 create table if not exists hbtn_0d_usa.states (
-    id INT IDENTITY(1,1) UNIQUE not null PRIMARY KEY,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) not null
 );
