3db12c879f34e19e3e85afe7cc368f04e2df9963
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-31 10:23:17 +0100

    swapping most of shows by genre

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 --  all Comedy shows in the database hbtn_0d_tvshows
-SELECT tv_genres.name FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.name = "Comedy"
-ORDER BY tv_genres.title;
+SELECT tv_genres.title FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
