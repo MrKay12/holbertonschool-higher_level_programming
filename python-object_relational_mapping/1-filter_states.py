e1340cbad152a7746ca799a427f36b478e2f38dc
MrKay12 <quentin.lepoutre12@gmail.com>
2024-11-01 19:12:45 +0100

    changing execute back

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -16,8 +16,7 @@ if __name__ == "__main__":
         port=3306
         )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE \
-                'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         if row[1][0] == 'N':
