c33539a9b093386ce23046e08e6cc0dd5acc9b1b
MrKay12 <quentin.lepoutre12@gmail.com>
2024-11-01 19:11:40 +0100

    adding a if

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -16,9 +16,11 @@ if __name__ == "__main__":
         port=3306
         )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE \
+                'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
     cur.close()
     db.close()
