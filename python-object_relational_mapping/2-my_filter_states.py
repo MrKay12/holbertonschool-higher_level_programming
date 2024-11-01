e7633da2f1de7237c52f6ebb99ca044e433669ee
MrKay12 <quentin.lepoutre12@gmail.com>
2024-11-01 19:01:37 +0100

    cutting line

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
         port=3306
         )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name = %s \
+                ORDER BY id ASC", (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
