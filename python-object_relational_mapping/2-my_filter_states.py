fdfb8a37606df83ee1e97d68dbd886a7a736baf2
MrKay12 <quentin.lepoutre12@gmail.com>
2024-11-01 19:33:56 +0100

    if

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
                 ORDER BY id ASC", (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1] == sys.argv[4]:
+            print(row)
     cur.close()
     db.close()
