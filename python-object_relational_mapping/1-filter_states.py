bd9f5153424716ea125e3cb4cb3e91ebf3474f25
MrKay12 <quentin.lepoutre12@gmail.com>
2024-11-01 19:31:07 +0100

    chmod

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3],
         port=3306
-        )
+    )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     states = cur.fetchall()
